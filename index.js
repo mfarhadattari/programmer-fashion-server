ef5b0cc99a58209297d66442fec7a0deedf132b4
Mohammad Farhad <mfarhad.attari@gmail.com>
2023-07-03 19:51:21 +0600

    basic setup and instailliation and also mongodb connect

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// ! Middleware
+app.use(cors());
+app.use(express.json());
+
+const client = new MongoClient(process.env.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    client.connect();
+    //! Code here
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Programmer Fashion Server is Running");
+});
+
+app.listen(port, () => {
+  console.log(`Programmer Fashion Server is Running on port ${port}`);
+});
---
+++ readme.md
@@ -0,0 +1,3 @@
+
+
+### https://programmer-fashion.vercel.app/
