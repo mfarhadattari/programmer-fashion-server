29b72a343214bbd36da9d333588182400094f8a2
mfarhadattari <mfarhad.attari@gmail.com>
2023-07-24 17:42:50 +0600

    product deleting functionality added

--- routers/admin.js
+++ routers/admin.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { jwtVerify, adminVerify } = require("../middlewares/middlewares");
 const router = express.Router();
+const {ObjectId} = require("mongodb")
 
 router.get("/all-products", jwtVerify, adminVerify, async (req, res) => {
   const productCollection = req.productCollection;
@@ -9,6 +10,13 @@ router.get("/all-products", jwtVerify, adminVerify, async (req, res) => {
     .sort({ timeData: -1 })
     .toArray();
   res.send(products);
+});
+
+router.delete("/delete-product/:id", jwtVerify, adminVerify, async (req, res) => {
+  const productCollection = req.productCollection;
+  const query = { _id: new ObjectId(req.params.id) };
+  const deletedResult = await productCollection.deleteOne(query);
+  res.send(deletedResult);
 });
 
 module.exports = router;
